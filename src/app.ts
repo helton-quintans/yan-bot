0be4e469c9b2c7ef4bfdee37698bdc6099927171
Helton Quintãns <helton.quit@gmail.com>
2021-10-16 23:48:47 -0300

    Envio da promoção do dia de TER a DOM

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 // Supports ES6
+
 import { create, Whatsapp } from "venom-bot";
 import manager from "./utils/manager";
 // const { logger } = require("./config/logger");
+import { format, compareAsc } from "date-fns";
 
 const Main = async () => {
   const nlpManager = manager.config();
@@ -25,14 +27,74 @@ const Main = async () => {
               // );
               //  break;
               //case "saudacao":
-                //await client.sendImage();
-                //break;
+              //await client.sendImage();
+              //break;
               case "menu":
                 await client.sendText(message.from, response.answer);
                 break;
               //case "promocao":
-                //await client.sendImage(`+5581996591072@c.br`,);
-                //break;
+              //await client.sendImage(`+5581996591072@c.br`,);
+              //break;
+              case "promocao":
+                const today = format(new Date(), "EEE");
+                console.log(today);
+                try {
+                  switch (today) {
+                    case "Sat":
+                      await client.sendImage(
+                        message.from,
+                        "C:/Projetos/yan-bot/src/images/sat.png",
+                        "sat.png",
+                        "Promoção do sabaãaao caraaiooo, porra"
+                      );
+                      break;
+                    case "Sun":
+                      await client.sendImage(
+                        message.from,
+                        "C:/Projetos/yan-bot/src/images/sun.png",
+                        "Sun.png",
+                        "Promoção do domingãaao caraaiooo, porra"
+                      );
+                      break;
+                    case "Tue":
+                      await client.sendImage(
+                        message.from,
+                        "C:/Projetos/yan-bot/src/images/tue.png",
+                        "Teu.png",
+                        "Promoção do *teeerça* caraaiooo, porra"
+                      );
+                      break;
+                    case "Wed":
+                      await client.sendImage(
+                        message.from,
+                        "C:/Projetos/yan-bot/src/images/wed.png",
+                        "Wed.png",
+                        "Promoção do *Quaaarta* caraaiooo, porra"
+                      );
+                      break;
+                    case "Thu":
+                      await client.sendImage(
+                        message.from,
+                        "C:/Projetos/yan-bot/src/images/thu.png",
+                        "Thu.png",
+                        "Promoção do *Quiiiinta* caraaiooo, porra"
+                      );
+                      break;
+                    case "Fri":
+                      await client.sendImage(
+                        message.from,
+                        "C:/Projetos/yan-bot/src/images/sun.png",
+                        "Sun.png",
+                        "Promoção de *Seeexta* caraaiooo, porra"
+                      );
+                      break;
+                    default:
+                      break;
+                  }
+                } catch (error) {
+                  console.error(error);
+                }
+                break;
               case "localizacao":
                 await client.sendLocation(
                   message.from,
@@ -42,7 +104,10 @@ const Main = async () => {
                 );
                 break;
               default:
-                const formatedResponse = response.answer.replace("nome-do-cliente", message.sender.shortName)
+                const formatedResponse = response.answer.replace(
+                  "nome-do-cliente",
+                  message.sender.shortName
+                );
                 await client.sendText(message.from, formatedResponse);
                 break;
             }
